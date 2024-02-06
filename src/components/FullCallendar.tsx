6d9ba207398659668fed28a9325522b0b7eef333
arashjp8 <arashjafarpour723@gmail.com>
2024-02-06 16:22:00 +0330

    [checkpoint] getting familiar with different initialviews

--- src/components/FullCallendar.tsx
+++ src/components/FullCallendar.tsx
@@ -1,16 +1,38 @@
 import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import listWeek from "@fullcalendar/list";
+import dayGrid from "@fullcalendar/daygrid";
+import timeGrid from "@fullcalendar/timegrid";
 
 export const FullCallendar = () => {
   return (
-    <FullCalendar
-      plugins={[dayGridPlugin]}
-      initialView={"dayGridMonth"}
-      // weekends={false}
-      events={[
-        { title: "event 1", date: "2024-02-08" },
-        { title: "event 2", date: "2024-02-09" },
-      ]}
-    />
+    <>
+      <FullCalendar
+        plugins={[timeGrid]}
+        initialView={"timeGridWeek"}
+        // weekends={false}
+        events={[
+          { title: "event 1", date: "2024-02-08" },
+          { title: "event 2", date: "2024-02-09" },
+        ]}
+      />
+      {/*  <FullCalendar*/}
+      {/*  plugins={[listWeek]}*/}
+      {/*  initialView={"listWeek"}*/}
+      {/*  // weekends={false}*/}
+      {/*  events={[*/}
+      {/*    { title: "event 1", date: "2024-02-08" },*/}
+      {/*    { title: "event 2", date: "2024-02-09" },*/}
+      {/*  ]}*/}
+      {/*/>*/}
+      {/*<FullCalendar*/}
+      {/*  plugins={[dayGrid]}*/}
+      {/*  initialView={"dayGridMonth"}*/}
+      {/*  // weekends={false}*/}
+      {/*  events={[*/}
+      {/*    { title: "event 1", date: "2024-02-08" },*/}
+      {/*    { title: "event 2", date: "2024-02-09" },*/}
+      {/*  ]}*/}
+      {/*/>*/}
+    </>
   );
 };
